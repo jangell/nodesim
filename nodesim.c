17d0a37770ead7393ceaeeb645f4a7dc5ae4f3c9
Jason Angell <waterboy94@gmail.com>
2014-12-31 19:47:26 -0500

    IT WORKS LETS GO

--- nodesim.c
+++ nodesim.c
@@ -3,26 +3,38 @@
 // started 12/28/2014
 // last edited 12/28/2014
 
+// see end of file for description of grid & arcgrid
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "simulations.h"
 
 #define GRIDSIZEX 70
 #define GRIDSIZEY 27
+#define ARCSIZEX GRIDSIZEX * 2
+#define ARCSIZEY GRIDSIZEY * 2
 
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_BLUE    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_RED 		"\x1b[31m" // target node
+#define ANSI_COLOR_BLUE   "\x1b[36m" // node path
+#define ANSI_COLOR_GREEN 	"\x1b[32m" // djikstra
+#define ANSI_COLOR_RESET  "\x1b[0m"
+
+#define ARCGRID(i, j)  (arcgrid + (j * ARCSIZEX) + i)
+#define GRID(i, j)     (grid + (j * GRIDSIZEX) + i)
 
 int printgrid();
 int gridinit();
 
 int main(int argc, char * argv[])
 {
+  srandom(time(NULL));
   int * grid = malloc(sizeof(int) * GRIDSIZEX * GRIDSIZEY);
+  int * arcgrid = malloc(sizeof(int) * ARCSIZEX * ARCSIZEY);
   gridinit(grid, GRIDSIZEX, GRIDSIZEY);
-  randomfinder(grid, GRIDSIZEX, GRIDSIZEY, 4, 5, 28, 13);
-  //printgrid(grid, GRIDSIZEX, GRIDSIZEY);
+  gridinit(arcgrid, ARCSIZEX, ARCSIZEY);
+  randomfinder(grid, arcgrid, GRIDSIZEX, GRIDSIZEY, 10, 10, 20, 20);
+  //printgrid(grid, arcgrid, GRIDSIZEX, GRIDSIZEY);
   return 0;
 }
 
@@ -32,43 +44,88 @@ int gridinit(int * grid, int sizex, int sizey)
   int i, j;
   for(i = 0; i < sizey; i++){
     for(j = 0; j < sizex; j++){
-      *(grid + (GRIDSIZEX * i) + j) = 0;
+      *(GRID(j, i)) = 0; // note that the j, i is because the loop is nested opposite of the lower loop, where it is i, j
     }
   }
   return 0;
 }
 
-int printgrid(int * grid, int sizex, int sizey)
+int printgrid(int * grid, int * arcgrid, int sizex, int sizey)
 {
-  int i, j;
-  for(j = 0; j < sizey; j++){
-    for(i = 0; i < sizex; i++){
-      int val = *(grid + (j * GRIDSIZEX) + i);
+  int i, j, k;
+  k = 0;
+  for(j = 0; j < GRIDSIZEY; j++){
+    for(i = 0; i < GRIDSIZEX; i++){
+      int val = *GRID(i, j);
       if(val == 0){
 	printf("o");
       }
       else if(val == 1){
-	printf(ANSI_COLOR_RED "x" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE "x" ANSI_COLOR_RESET);
       }
       else if(val == 2){
-	printf(ANSI_COLOR_BLUE "@" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "@" ANSI_COLOR_RESET);
+      }
+      else if(val == 3){
+      	printf(ANSI_COLOR_GREEN "+" ANSI_COLOR_RESET);
       }
       else{
 	printf("%i\n", val);
 	return 1;
       }
-      if(i < (sizex - 1)){
-	printf("-");
+      if(*ARCGRID(i, k) == 1){
+				printf(ANSI_COLOR_BLUE "-" ANSI_COLOR_RESET);
+      }
+      else if(*ARCGRID(i, k) == 0){
+      	printf(" ");
+      }
+      else{
+      	printf("%i", *ARCGRID(i, k));
       }
     }
+    k++;
     printf("\n");
-    if(j < sizey - 1){
-      for(i = 0; i < sizex - 1; i++){
-	printf("| ");
-      }
-      printf("|");
+    // print arcs based on arcgrid
+    for(i = 0; i < ARCSIZEX; i++){
+    	if(*ARCGRID(i, k) == 1){
+    		printf(ANSI_COLOR_BLUE "/" ANSI_COLOR_RESET);
+    	}
+    	else if(*ARCGRID(i, k) == 2){
+    		printf(ANSI_COLOR_BLUE "\\" ANSI_COLOR_RESET);
+    	}
+    	else if(*ARCGRID(i, k) == 3){
+    		printf(ANSI_COLOR_BLUE "X" ANSI_COLOR_RESET);
+    	}
+    	else if(*ARCGRID(i, k) == 4){
+    		printf(ANSI_COLOR_BLUE "|" ANSI_COLOR_RESET);
+    	}
+    	else{
+    		printf(" ");
+    	}
     }
+    k++;
     printf("\n");
   }
   return 0;
 }
+
+/* grid looks like:
+
+000110
+001010
+111001
+100010
+000100
+
+*/
+
+/* arcgrid looks like:
+
+111001000000
+203102301120
+011000000000
+101221231123
+111000000000
+203100221001
+
+*/
--- random.c
+++ random.c
@@ -8,49 +8,119 @@
 #include <time.h>
 #include "tools.h"
 
-#define gridspot (grid + (sizex * ony) + onx)
+#define GRIDSIZEX sizex
+#define GRIDSIZEY sizey
+#define ARCSIZEX GRIDSIZEX * 2
+#define ARCSIZEY GRIDSIZEY * 2
 
-int randomfinder(int * grid, int sizex, int sizey, int startx, int starty, int endx, int endy)
+#define GRIDSPOT(x, y) (grid + (GRIDSIZEX * (y)) + x)
+#define ARCSPOT(x, y)  (arcgrid + (ARCSIZEX * (y)) + x)
+
+int randomfinder(int * grid, int * arcgrid, int sizex, int sizey, int startx, int starty, int endx, int endy)
 {
   int onx, ony;
   onx = startx;
   ony = starty;
-  *(grid + (sizex * endy) + endx) = 2;
+  *GRIDSPOT(endx, endy) = 2;
   srandom(time(NULL));
   int lastx = onx;
   int lasty = ony;
   int killcount = 0;
+  int time = 0;
+  int moves = 0;
   while(onx != endx || ony != endy){
     // everything happens here
-    int currand = random()%4;
+  	// editing the arc grid is frustrating because of all the arithmetic, BUT:
+  		// the order is important; the values that are used depend on what operation(s)
+  		// have already happened, so be careful with them
+    int currand = random()%8;
     if(currand == 0 && onx < sizex - 1){
-      onx++;
-      if(*gridspot == 1){
-	onx--;
+      if(*GRIDSPOT(onx+1, ony) == 0){
+      	onx++;
+      	*ARCSPOT(onx-1, ony*2) = 1;
       }
     }
     else if(currand == 1 && onx > 0){
-      onx--;
-      if(*gridspot == 1){
-	onx++;
-      }
+			if(*GRIDSPOT(onx-1, ony) == 0){
+				onx--;
+				*ARCSPOT(onx, ony*2) = 1;
+			}
     }
     else if(currand == 2 && ony < sizey - 1){
-      ony++;
-      if(*gridspot == 1){
-	ony--;
+    	if(*GRIDSPOT(onx, ony+1) == 0){
+    		*ARCSPOT(onx*2, ony*2+1) = 4;
+      	ony++;
       }
     }
     else if(currand == 3 && ony > 0){
-      ony--;
-      if(*gridspot == 1){
-	ony++;
+    	if(*GRIDSPOT(onx, ony-1) == 0){
+    		*ARCSPOT(onx*2, ony*2-1) = 4;
+      	ony--;
       }
     }
+  	// 2 3
+    else if(currand == 4 && onx > 0 && ony > 0){
+    	int sym = *ARCSPOT(onx*2-1, ony*2-1);
+    	if((sym == 0 || sym == 1) && *GRIDSPOT(onx - 1, ony - 1) == 0){
+    		if(sym == 0){
+      		*ARCSPOT(onx*2-1, ony*2-1) = 2;
+      	}
+      	else{
+      		*ARCSPOT(onx*2-1, ony*2-1) = 3;
+      	}
+    		onx--;
+      	ony--;
+    	}
+    }
+    else if(currand == 5 && onx < sizex - 1 && ony > 0){
+    	int sym = *ARCSPOT(onx*2+1, ony*2-1);
+    	if((sym == 0 || sym == 2) && *GRIDSPOT(onx + 1, ony - 1) == 0){
+      	if(sym == 0){
+      		*ARCSPOT(onx*2+1, ony*2-1) = 1;
+      	}
+      	else{
+      		*ARCSPOT(onx*2+1, ony*2-1) = 3;
+      	}
+      	onx++;
+      	ony--;
+      }
+    }
+    else if(currand == 6 && onx > 0 && ony < sizey - 1){
+    	int sym = *ARCSPOT(onx*2-1, ony*2+1);
+    	if((sym == 0 || sym == 2) && *GRIDSPOT(onx - 1, ony + 1) == 0){
+      	if(sym == 0){
+      		*ARCSPOT(onx*2-1, ony*2+1) = 1;
+      	}
+      	else{
+      		*ARCSPOT(onx*2-1, ony*2+1) = 3;
+      	}
+      	onx--;
+      	ony++;
+      }
+    }
+    else if(currand == 7 && onx < sizex - 1 && ony < sizey - 1){
+    	int sym = *ARCSPOT(onx*2+1, ony*2+1);
+    	if((sym == 0 || sym == 1) && *GRIDSPOT(onx + 1, ony + 1) == 0){
+      	if(sym == 0){
+      		*ARCSPOT(onx*2+1, ony*2+1) = 2;
+      	}
+      	else{
+      		*ARCSPOT(onx*2+1, ony*2+1) = 3;
+      	}
+      	onx++;
+      	ony++;
+      }
+    }
+    time++;
     if(onx == lastx && ony == lasty){
       killcount++;
-      if(killcount > 8){
-	printf("Looks like this thing lost the game");
+      if(killcount > 10){
+	printf("Starting point: [%i, %i]\n", startx, starty);
+	printf("Ending point:   [%i, %i]\n\n", endx, endy);
+	printf("Random:\n");
+	printf("DNF (terminated at [%i, %i])\n", onx, ony);
+	printf("\ttime =  %i\n", time);
+	printf("\tmoves = %i\n", moves);
 	return 1;
       }
     }
@@ -58,15 +128,21 @@ int randomfinder(int * grid, int sizex, int sizey, int startx, int starty, int e
       killcount = 0;
       lastx = onx;
       lasty = ony;
+      moves++;
     }
-    *gridspot = 1;
-    printgrid(grid, sizex, sizey);
+    *GRIDSPOT(onx, ony) = 1;
+    printgrid(grid, arcgrid, sizex, sizey);
     // sleep to make progression visible, using nanosleep
     struct timespec * sleeptime = malloc(sizeof(struct timespec));
-    sleeptime->tv_sec = 0;
-    sleeptime->tv_nsec = 50000000;
+    sleeptime->tv_sec = 1;
+    sleeptime->tv_nsec = 0000000;
     nanosleep(sleeptime, sleeptime);
   }
   // print stats
+  printf("Starting point: [%i, %i]\n", startx, starty);
+  printf("Ending point:   [%i, %i]\n\n", endx, endy);
+  printf("Random:\n");
+  printf("\ttime =  %i\n", time);
+  printf("\tmoves = %i\n", moves);
   return 0;
 }
